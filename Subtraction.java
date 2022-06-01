610b6e80831d8ef78eb061648c11bdbebc101bea
HRISHIKESH6026 <106652832+HRISHIKESH6026@users.noreply.github.com>
2022-06-01 13:18:23 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+/* programe to display subtraction
+of two numbers*/
+// programe to display the subtraction of two numbers
+          import java.io.*;
+          class Subtraction{
+          public static void main(String args[]){
+          int a=50, b=25,c;
+          c=a-b;
+          System.out.println(c);
+}
+} 
